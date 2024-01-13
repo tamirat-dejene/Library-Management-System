a1822af56c07ca7409424347074b268f074a6b93
Tamirat Dejene <tamirat.dejenie@gmail.com>
2024-01-13 22:27:13 +0300

    Delete source/Maven_Java_Projects/Library_Management_System directory

--- source/Maven_Java_Projects/Library_Management_System/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.lms.library_management_system {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires mysql.connector.j;
-    requires java.sql;
-    
-    opens com.lms.library_management_system to javafx.fxml;
-    exports com.lms.library_management_system;
-}
